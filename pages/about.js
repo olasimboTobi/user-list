bc0a8119be41927abfb57a7d9834c7002ab286d1
Decagon <great4christ2009@gmail.com>
2022-01-31 16:24:17 +0100

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div className="about">
+                <h1>About</h1>
+                <p>I’m seeing use of Java, C#, Go, Python in my area as the main server-side languages.</p>
+                <p>I’m seeing use of Java, C#, Go, Python in my area as the main server-side languages.</p>
+            </div>
+        </>
+        
+     );
+}
+ 
+export default About;
